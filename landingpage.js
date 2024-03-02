dc0e693625feacabd220dad09e45a8721ea54369
yūgu <118666092+geekyTWILIGHT@users.noreply.github.com>
2024-03-02 19:50:57 +0800

    Update landingpage.js

--- landingpage.js
+++ landingpage.js
@@ -4,3 +4,24 @@ function enterWebsite() {
 }
 
 setTimeout(enterWebsite, 4000);
+
+function moveToLandingPage() {
+    window.location.href = "index.html";
+}
+
+function copyURL() {
+    var url = window.location.href; 
+    var tempInput = document.createElement("input"); 
+    document.body.appendChild(tempInput);
+    tempInput.setAttribute("value", url); 
+    tempInput.select(); 
+    document.execCommand("copy"); 
+    document.body.removeChild(tempInput); 
+    
+    var copiedNotif = document.getElementById("link-copied-notif");
+    copiedNotif.style.opacity = 1; 
+
+    setTimeout(function() {
+      copiedNotif.style.opacity = 0; 
+    }, 2000);
+}
