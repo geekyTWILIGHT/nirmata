69f2a0adf9760d26083cae01b5ca417317889760
yūgu <118666092+geekyTWILIGHT@users.noreply.github.com>
2024-03-04 01:02:12 +0800

    Add files via upload

---
+++ contents/terms-of-service.js
@@ -0,0 +1,37 @@
+// Copies the current URL of the browser
+function copyURL() {
+    var url = window.location.href; 
+    var tempInput = document.createElement("input"); 
+    document.body.appendChild(tempInput);
+    tempInput.setAttribute("value", url); 
+    tempInput.select(); 
+    document.execCommand("copy"); 
+    document.body.removeChild(tempInput); 
+    
+    var copiedNotif = document.getElementById("link-copied-notif");
+    copiedNotif.style.opacity = 1; 
+
+    setTimeout(function() {
+      copiedNotif.style.opacity = 0; 
+    }, 2000);
+}
+
+// Go to top
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (window.scrollY > window.innerHeight * 0.5 && window.scrollY < document.documentElement.scrollHeight - window.innerHeight * 1.5) {
+      document.querySelector(".go-up").style.display = "flex";
+  } else {
+      document.querySelector(".go-up").style.display = "none";
+  }
+}
+
+function goToTop() {
+  window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+  });
+}
---
+++ locationModifier.js
@@ -0,0 +1,19 @@
+// Goes to the landing page
+function moveToLandingpage() {
+    window.location.href = "index.html";
+}
+
+// Goes to the landing page (For files excluding the landing page file)
+function toLandingpage() {
+    window.location.href = "../index.html";
+}
+
+// Goes to the terms of service page 
+function moveToTermsSerPage() {
+    window.location.href = "terms-of-service.html";
+}
+
+// Goes to the terms of service page (for landing page file)
+function toTermsSerPage() {
+    window.location.href = "contents/terms-of-service.html";
+}
