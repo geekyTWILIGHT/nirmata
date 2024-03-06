2fa3a6e4ca0e38e09aee1eb6d7a8a2d78beba1af
yūgu <118666092+geekyTWILIGHT@users.noreply.github.com>
2024-03-06 11:37:47 +0800

    Add files via upload

---
+++ locationModifier.js
@@ -0,0 +1,106 @@
+// Goes to the landing page
+function moveToLandingpage() {
+    window.location.href = "index.html";
+}
+
+// Goes to the landing page (For files excluding the landing page file)
+function toLandingpage() {
+    window.location.href = "../index.html";
+}
+
+
+// Goes to the about us page 
+function moveToAboutPage()  {
+    window.open("about-us.html", "_blank");
+}
+
+// Goes to the about us page (for landing and products page)
+function toAboutPage()  {
+    window.open("contents/about-us.html", "_blank");
+}
+
+
+// Goes to the our story page 
+function moveToOurStoryPage()  {
+    window.open("our-story.html", "_blank");
+}
+
+// Goes to the our story page (for landing and products page)
+function toOurStoryPage()  {
+    window.open("contents/our-story.html", "_blank");
+}
+
+
+// Goes to the contact us page 
+function moveToContactsPage()  {
+    window.open("contact-us.html", "_blank");
+}
+
+// Goes to the contact us page (for landing and products page)
+function toContactsPage()  {
+    window.open("contents/contact-us.html", "_blank");
+}
+
+
+// Goes to the privacy policy page 
+function moveToPrIPoPage() {
+    window.open("privacy-policy.html", "_blank");
+}
+
+// Goes to the privacy policy page (for landing and products page)
+function toPriPoPage() {
+    window.open("contents/privacy-policy.html", "_blank");
+}
+
+
+// Goes to the return policy page 
+function moveToRePoPage() {
+    window.open("return-policy.html", "_blank");
+}
+
+// Goes to the return policy page (for landing and products page)
+function toRePoPage() {
+    window.open("contents/return-policy.html", "_blank");
+}
+
+
+// Goes to the terms of service page 
+function moveToTermsSerPage() {
+    window.open("terms-of-service.html", "_blank");
+}
+
+// Goes to the terms of service page (for landing and products page)
+function toTermsSerPage() {
+    window.open("contents/terms-of-service.html", "_blank");
+}
+
+
+
+// Goes to product page 
+function moveToProducts() {
+    window.location.href = "products.html";
+}
+
+// Goes to product page (for landing and products page)
+function toProducts() {
+    window.location.href = "contents/products.html";
+}
+
+
+
+// Goes to email 
+function goEmail() {
+
+}
+
+
+// Goes to facebook page 
+function goFacebook() {
+
+}
+
+
+// Goes to twitter page 
+function goTwitter() {
+
+}
